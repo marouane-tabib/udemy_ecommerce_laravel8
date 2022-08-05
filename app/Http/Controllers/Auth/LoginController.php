9eb6c4c8c80c7be881faf01dab187e38cfa65495
marwan-tabib <marwanmaroc297@gmail.com>
2022-08-05 03:16:28 +0200

    22

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 
 class LoginController extends Controller
 {
@@ -50,5 +52,18 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * The user has logged out of the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    protected function loggedOut(Request $request)
+    {
+        Cache::forget('admin_side_menu');
+        Cache::forget('role_routes');
+        Cache::forget('user_routes');
     }
 }
--- app/Http/Middleware/Roles.php
+++ app/Http/Middleware/Roles.php
@@ -5,6 +5,7 @@ namespace App\Http\Middleware;
 use App\Models\Role;
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 
@@ -21,24 +22,39 @@ class Roles
     {
         $routeName = Route::getFacadeRoot()->current()->uri();
         $route = explode('/' , $routeName);
-        $roleRoutes = Role::distinct()->whereNotNull('allowed_route')
-                        ->pluck('allowed_route')->toArray();
+        //$roleRoutes = Role::distinct()->whereNotNull('allowed_route')->pluck('allowed_route')->toArray();
 
         if (auth()->check()){
-            if (!in_array($route[0] , $roleRoutes)){
+            if (!in_array($route[0] , $this->roleRoutes())){
                 return $next($request);
             }else{
-                if ($route[0] != auth()->user()->roles[0]->allowed_route){
-                    $path = $route[0] == auth()->user()->roles[0]->allowed_route ? $route[0].'.login' : '' . auth()->user()->roles[0]->allowed_route.'front.index';
+                if ($route[0] != $this->userRoutes()){
+                    $path = $route[0] == $this->userRoutes() ? $route[0].'.login' : '' . $this->userRoutes().'front.index';
                     return redirect()->route($path);
                 }else{
                     return $next($request);
                 }
             }
         } else{
-            $routeDestination = in_array($route[0] , $roleRoutes) ? $route[0].'.login' : 'login';
-            $path = $route[0] != '' ? $routeDestination : auth()->user()->roles[0]->allowed_route.'.index';
+            $routeDestination = in_array($route[0] , $this->roleRoutes()) ? $route[0].'.login' : 'login';
+            $path = $route[0] != '' ? $routeDestination : $this->userRoutes().'.index';
             return redirect()->route($path);
         }
+    }
+
+    protected function roleRoutes(){
+
+        if (!Cache::has('role_routes')) {
+            Cache::forever('role_routes' , Role::distinct()->whereNotNull('allowed_route')->pluck('allowed_route')->toArray());
+        }
+        return Cache::get('role_routes');
+    }
+
+    protected function userRoutes(){
+
+        if (!Cache::has('user_routes')) {
+            Cache::forever('user_routes' , auth()->user()->roles[0]->allowed_route);
+        }
+        return Cache::get('user_routes');
     }
 }
