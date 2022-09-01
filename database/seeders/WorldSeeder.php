8b8a2f2c93d5c324a02ea18b41d63415e9e95f45
marwan-tabib <marwanmaroc297@gmail.com>
2022-09-01 03:00:29 +0200

    37.2 & seeding world tables

--- database/seeders/WorldSeeder.php
+++ database/seeders/WorldSeeder.php
@@ -23,12 +23,15 @@ class WorldSeeder extends Seeder
             'username' => env('DB_USERNAME'),
             'password' => env('DB_PASSWORD'),
             'host' => env('DB_HOST'),
-            'database' => env('DB_DATABASE')
+            'database' => env('DB_DATABASE'),
         ];
+
+        // You must change this one, its depend on your mysql bin.
+        $db_bin = 'C:\xampp\mysql\bin';
         
        //$sql = storage_path('a_id_territory.sql');
 
-       exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database {$db['database']} < $countries");
+       exec("{$db_bin}\mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database={$db['database']} < $world");
 
         // exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database={$db['database']} < $countries");
         // exec("mysql --user={$db['username']} --password={$db['password']} --host={$db['host']} --database={$db['database']} < $states");
